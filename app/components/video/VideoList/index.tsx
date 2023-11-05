6e996d5f49cbdffbd694eae8ed864819cad1886f
Visualizeit <17688172112@163.com>
2023-11-05 20:58:02 +0800

    feat: add ScrollToTopButton

--- app/components/video/VideoList/index.tsx
+++ app/components/video/VideoList/index.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { Box, ScrollArea, Stack } from '@mantine/core'
 import { useElementSize, useIntersection } from '@mantine/hooks'
 import { useQueryState } from 'next-usequerystate'
 import { useSuspenseInfiniteQuery } from '@tanstack/react-query'
 import videoAPI from '@/app/apis/videoAPI'
 import VideoListSection from '../VideoListSection'
+import ScrollToTopButton from './ScrollToTopButton'
 
 const VideoList = () => {
   const [search] = useQueryState('search')
@@ -20,9 +21,11 @@ const VideoList = () => {
     ...videoAPI.getVideoList({ search, categoryId })
   })
 
-  const { ref: sizeRef, width } = useElementSize()
+  const { ref: sizeRef, width } = useElementSize<HTMLDivElement>()
 
   const { ref: intersectionRef, entry } = useIntersection()
+
+  const [mounted, setMounted] = useState(false)
 
   useEffect(() => {
     if (entry?.isIntersecting && !isFetching) {
@@ -30,19 +33,33 @@ const VideoList = () => {
     }
   }, [entry, isFetching, fetchNextPage])
 
+  const handleScrollTop = () => {
+    sizeRef.current?.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
   return (
-    <ScrollArea className="flex-1 rounded-lg" viewportRef={sizeRef}>
+    <ScrollArea
+      className="flex-1 rounded-lg"
+      viewportRef={sizeRef}
+      onScrollPositionChange={position => {
+        setMounted(position.y > 100)
+      }}>
       <Stack gap="xs">
-        {width
-          ? pages.map(page => (
-              <VideoListSection
-                key={page.currentPage}
-                videos={page.list}
-                width={width}
-              />
-            ))
-          : null}
+        {useMemo(
+          () =>
+            width
+              ? pages.map(page => (
+                  <VideoListSection
+                    key={page.currentPage}
+                    videos={page.list}
+                    width={width}
+                  />
+                ))
+              : null,
+          [width, pages]
+        )}
       </Stack>
+      <ScrollToTopButton mounted={mounted} onScrollToTop={handleScrollTop} />
       <Box className="h-px" ref={intersectionRef} />
     </ScrollArea>
   )
