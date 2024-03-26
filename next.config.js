5a63cdc98a4b9c338b965bd92976f3942d4f3726
xiaoliverygood <ljzcomeon@gmail.com>
2024-03-26 19:36:14 +0800

    dockerui

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   images: {
+//     remotePatterns: [{ hostname: '*.hn-bkt.clouddn.com' }]
+//   }
+// }
+//
+// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    remotePatterns: [{ hostname: '*.hn-bkt.clouddn.com' }]
-  }
-}
-
-module.exports = nextConfig
+module.exports = {
+  output: "standalone",
+};
